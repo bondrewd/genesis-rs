11e3de6feeeebc1dafbbd17c89a963bc59ce9dd5
Diego Ugarte <diego.ugarte@outlook.com>
2024-10-19 12:06:53 +0900

    Implement MD basic features
    [NEW]
    - implement potential energy calculation
    - implement kinetic energy calculation
    
    [CHANGE]
    - change time step to 1e-5
    - accumulate energies using f64
    
    [FIX]
    - fix bugs in force calculation

--- src/main.rs
+++ src/main.rs
@@ -6,15 +6,15 @@ fn compute_force(r: &Vec<[f32; 4]>, f: &mut Vec<[f32; 3]>, e: f32, s: f32) {
 
     for i in 0..r.len() {
         f[i] = [0.0, 0.0, 0.0];
-        for j in 0..r.len() {
+        for j in (i + 1)..r.len() {
             if i != j {
                 let dr = [r[j][0] - r[i][0], r[j][1] - r[i][1], r[j][2] - r[i][2]];
                 let r2 = 1.0_f32 / (dr[0] * dr[0] + dr[1] * dr[1] + dr[2] * dr[2]);
                 let c2 = s2 * r2;
                 let c4 = c2 * c2;
-                let c8 = c4 * c4;
+                let c6 = c4 * c2;
 
-                let force = 48.0 * e * c8 * c4 * (c2 - 0.5) * r2;
+                let force = 48.0 * e * c6 * (c6 - 0.5) * r2;
 
                 f[i][0] -= force * dr[0];
                 f[i][1] -= force * dr[1];
@@ -26,6 +26,38 @@ fn compute_force(r: &Vec<[f32; 4]>, f: &mut Vec<[f32; 3]>, e: f32, s: f32) {
             }
         }
     }
+}
+
+fn compute_potential_energy(r: &Vec<[f32; 4]>, e: f32, s: f32) -> f64 {
+    let mut potential_energy = 0.0_f64;
+    let s2 = s * s;
+
+    for i in 0..r.len() {
+        for j in (i + 1)..r.len() {
+            if i != j {
+                let dr = [r[j][0] - r[i][0], r[j][1] - r[i][1], r[j][2] - r[i][2]];
+                let r2 = 1.0_f32 / (dr[0] * dr[0] + dr[1] * dr[1] + dr[2] * dr[2]);
+                let c2 = s2 * r2;
+                let c4 = c2 * c2;
+                let c6 = c4 * c2;
+
+                potential_energy += (4.0 * e * c6 * (c6 - 1.0)) as f64;
+            }
+        }
+    }
+
+    potential_energy
+}
+
+fn compute_kinetic_energy(v: &Vec<[f32; 3]>, m: &Vec<f32>) -> f64 {
+    let mut kinetic_energy = 0.0_f64;
+
+    for i in 0..v.len() {
+        kinetic_energy +=
+            (0.5 * m[i] * (v[i][0] * v[i][0] + v[i][1] * v[i][1] + v[i][2] * v[i][2])) as f64;
+    }
+
+    kinetic_energy
 }
 
 fn main() {
@@ -49,7 +81,7 @@ fn main() {
         v[i][2] = rng.gen_range(-1.0..1.0);
     }
 
-    let dt = 0.01_f32;
+    let dt = 0.00001_f32;
     let dt_half = dt * 0.5;
     let n_steps = 1000;
     for _ in 0..n_steps {
@@ -70,9 +102,13 @@ fn main() {
             r[i][1] += v[i][1] * dt_half;
             r[i][2] += v[i][2] * dt_half;
         }
-    }
 
-    println!("r = {:?}", r);
-    println!("v = {:?}", v);
-    println!("f = {:?}", f);
+        let potential_energy = compute_potential_energy(&r, e, s);
+        let kinetic_energy = compute_kinetic_energy(&v, &m);
+        let total_energy = potential_energy + kinetic_energy;
+        println!(
+            "Total energy: {} (potential: {}, kinetic: {})",
+            total_energy, potential_energy, kinetic_energy,
+        );
+    }
 }
